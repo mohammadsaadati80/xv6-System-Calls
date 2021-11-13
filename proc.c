f1f1156de0f5286b21557cac49d2c5a35f9e3934
Emad Emami <emamiemad8@gmail.com>
2021-11-13 20:23:34 +0330

    sum of digits sys call

--- proc.c
+++ proc.c
@@ -531,3 +531,14 @@ procdump(void)
     cprintf("\n");
   }
 }
+
+int
+calculate_sum_of_digits(int number)
+{
+  int result = 0;
+  while(number) {
+    result += number % 10;
+    number /= 10;
+  }
+  return result;
+}
--- user.h
+++ user.h
@@ -23,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int calculate_sum_of_digits(void);
 
 // ulib.c
 int stat(const char*, struct stat*);
